83063aa4a7d79fb6574d87ec199637a900a6cd93
Jessica Aliberti <jmaliberti@gmail.com>
2024-03-07 12:57:56 -0800

    Added function with one parameter.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 var startMyDay = function () {
     return "Time walk the dog and eat breakfast!"
-}
+};
+console.log(startMyDay());
+
+var favoriteCookie = function (cookie) {
+    return "My favorite cookie is ${cookie}"
+};
+console.log(favoriteCookie());
+favoriteCookie("snickerdoodle");
+
