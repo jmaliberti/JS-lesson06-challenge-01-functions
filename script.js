a226820a2a8d41454b1c207cac96e259e4f74ffb
Jessica Aliberti <jmaliberti@gmail.com>
2024-03-08 12:55:21 -0800

    Added function with two parameters successfully

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ var startMyDay = function () {
     return "Time walk the dog and eat breakfast!"
 };
 console.log(startMyDay());
-
+//
 var favoriteCookie = function (cookie) {
     return `My favorite cookie is ${cookie}.`;
 };
 console.log(favoriteCookie("snickerdoodle"));
+//
+var introduce = function (name, occupation) {
+    return `My name is ${name}, and I am a ${occupation}.`
+};
+console.log(introduce("Jessica", "Leasing Admin"));
+console.log(introduce("David", "Bartender"));
